d63326fe4529f6e4fede66d145ae45d021ac203c
txrxso <teresaqwu@gmail.com>
2026-02-18 00:15:51 -0800

    removed unused comments

--- include/mqtt_config.h
+++ include/mqtt_config.h
@@ -3,10 +3,6 @@
 
 const char* MQTT_SERVER_HIVEMQ_PUBLIC = "broker.hivemq.com"; //"test.mosquitto.org";
 
- //"test.mosquitto.org";  - works
- //"10.0.0.115"; - home wifi with local broker running on computer
- //"206.87.235.56" - will not work - eduroam seems to block p2p? 
- //"fe26426fbe64463790fc2792777c8189.s1.eu.hivemq.cloud" - Hive MQ cluster 
 const int MQTT_PORT_HIVEMQ_PUBLIC = 1883; 
 const char* MQTT_TOPIC_ALERTS = "igen430/shh/alerts/workerA";
 const char* MQTT_TOPIC_HEARTBEATS = "igen430/shh/heartbeats/workerA";
