393cbb9e76c4fd9fbf16446404328051d798de85
txrxso <teresaqwu@gmail.com>
2026-02-21 17:10:09 -0800

    only print timeout on bus if CAN_DEBUG == 2

--- include/debug.h
+++ include/debug.h
@@ -1,10 +1,12 @@
 #ifndef DEBUG_H
 #define DEBUG_H
 
-#define IMU_DEBUG 0 // 2 for higher level 
+#define IMU_DEBUG 0 
+// 2 for higher level IMU printing
 #define GPS_DEBUG 1
 #define MQTT_DEBUG 1 
 #define CAN_DEBUG 1
+// set CAN_DEBUG to 2 to also print when no message received within timeout window, which can be useful for monitoring heartbeat timeouts, etc.
 #define HEARTBEAT_DEBUG 1
 
 
