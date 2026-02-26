e50b20d59ef48685cb5859142dfa644205fbec3b
txrxso <teresaqwu@gmail.com>
2026-02-26 13:33:10 -0800

    set DNS for hotspot option

--- src/wifi_config.cpp
+++ src/wifi_config.cpp
@@ -3,7 +3,7 @@
 void connectToWifi() {
     int attempts = 0;
     WiFi.begin(HOTSPOT_SSID, HOTSPOT_PSWD);
-    // WiFi.config(INADDR_NONE, INADDR_NONE, INADDR_NONE, IPAddress(8,8,8,8));
+    WiFi.config(INADDR_NONE, INADDR_NONE, INADDR_NONE, IPAddress(8,8,8,8));
     while (WiFi.status() != WL_CONNECTED) {
         delay(500);
         Serial.println("Connecting to Wifi...");
